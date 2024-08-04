dc6b1434b0e7fb7991966a3464680b2119078b59
vijayguptagh <120735063+vijayguptagh@users.noreply.github.com>
2024-08-04 10:15:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import React from "react";
-import Destinations from "./components/Destinations";
-import DownloadApp from "./components/DownloadApp";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Offer from "./components/Offer";
-import ScrollToTop from "./components/ScrollToTop";
-import Services from "./components/Services";
-import Testimonial from "./components/Testimonial";
-import Tours from "./components/Tours";
+import React from 'react';
+import { Destinations, DownloadApp, Footer, Home, Navbar, Offer, ScrollToTop, Services, Testimonial, Tours } from './components';
 
-export default function App() {
-  return (
-    <div>
-      <ScrollToTop />
-      <Navbar />
+const App = () => (
+  <main>
+    <ScrollToTop />
+    <Navbar />
+    <section>
       <Home />
       <Services />
       <Destinations />
@@ -22,7 +13,9 @@ export default function App() {
       <Tours />
       <Testimonial />
       <DownloadApp />
-      <Footer />
-    </div>
-  );
-}
+    </section>
+    <Footer />
+  </main>
+);
+
+export default App;
